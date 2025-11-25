84a290cdbb93584d56d9a31fa20267c00dc6cdb3
anwar <dev.anwarul@gmail.com>
2025-11-25 10:45:14 +0600

    C-03

--- module-11/src/server.ts
+++ module-11/src/server.ts
@@ -5,7 +5,7 @@ import config from "./config";
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("server is running.....");
-
+    //  root rout
     if (req.url == "/" && req.method == "GET") {
       res.writeHead(StatusCodes.OK, { "content-type": "application/json" });
       res.end(
@@ -14,6 +14,32 @@ const server: Server = http.createServer(
           path: req.url,
         })
       );
+    }
+    //  health rout
+    if (req.url == "/api" && req.method == "GET") {
+      res.writeHead(StatusCodes.OK, { "content-type": "application" });
+      res.end(
+        JSON.stringify({
+          message: "Health status ook ",
+          path: req.url,
+        })
+      );
+    }
+    //  users rout
+    if (req.url == "/api/users" && req.method == "POST") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+      req.on("end", () => {
+        try {
+          const pearsBody = JSON.parse(body);
+          console.log(pearsBody);
+          res.end(JSON.stringify(pearsBody));
+        } catch (error: any) {
+          console.log(error.message);
+        }
+      });
     }
   }
 );
